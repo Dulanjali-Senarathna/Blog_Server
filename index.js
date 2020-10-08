025c6f0b888b53c526fb569ea8f8db3de1774ec4
Dulanjali Senarathna <59603716+Dulanjali-Senarathna@users.noreply.github.com>
2020-10-08 20:09:55 +0530

    Connect the MongoDB database with NodeJs

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require ("express")
-const app = express()
+const express = require ("express");
+const mongoose = require ("mongoose");
 
-const Port = process.env.port || 5000
+mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
+
+const app = express();
+
+const connection = mongoose.connection;
+connection.once("open",() => {
+    console.log("Mongodb Connected");
+})
+
+const Port = process.env.port || 5000;
 
 app.route("/").get((req,res)=>res.json("your first rest api 1"));
 
